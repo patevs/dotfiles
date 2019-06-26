7bf2d2da083fb9f097460d0e8ab647b4679ff549
PatEvs <ppevans11@gmail.com>
2019-06-26 12:57:30 +1200

    ✨  Created bash/ powershell/ installs/

---
+++ installs/installs.md
@@ -0,0 +1,54 @@
+# Installs
+
+> Development programs and software installs
+
+---
+
+## Programs & Software
+
+- [`Git`](https://git-scm.com/download/win)
+- [`Chocolatey`](https://chocolatey.org/install)
+
+### Chocolatey Packages
+
+- **Terminal/Console**
+
+  - [`ConEmu`](https://chocolatey.org/packages/ConEmu)
+
+- **IDE's**
+
+  - [`Android Studio`](https://chocolatey.org/packages/AndroidStudio)
+  - [`SublimeText3`](https://chocolatey.org/packages/SublimeText3)
+  - [`Vim`](https://chocolatey.org/packages/vim)
+  - [`VSCode`](https://chocolatey.org/packages/vscode)
+
+- **Languages/Frameworkds**
+
+  - [`Java (jdk8)`](https://chocolatey.org/packages/jdk8)
+  - [`Python3`](https://chocolatey.org/packages/python/3.7.2)
+  - [`Node Version Manager (NVM)`](https://chocolatey.org/packages/nvm.portable)
+  - [`Go`](https://chocolatey.org/packages/golang)
+  - [`Hugo`](https://chocolatey.org/packages/hugo)
+
+- **Tools/Utilities**
+  - [`VS 2017 Build Tools`](https://chocolatey.org/packages/visualstudio2017buildtools)
+  - [`CCleaner`](https://chocolatey.org/packages/ccleaner)
+  - [`Revo Uninstaller`](https://chocolatey.org/packages/revo-uninstaller)
+
+---
+
+## `bat` Install & Setup
+
+- [`sharkdp/bat`](https://github.com/sharkdp/bat)
+
+To install `bat` run the following:
+
+```powershell
+# Install with chocolately
+$ choco install bat -y
+# Verify installation
+$ bat --version # or $ bat -V
+# bat 0.11.0
+```
+
+---
---
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,91 @@
+# -------------------------------- #
+#                                  #
+# ------ POWERSHELL PROFILE ------ #
+#                                  #
+#          @author patevs          #
+# -------------------------------- #
+
+#C:\tools\cmdermini\icons\cmder.ico
+
+# Import Posh-Git module
+Import-Module -Name posh-git
+
+# Start SSH agent
+Start-SshAgent
+
+# Setup alias'
+Set-Alias -Name gs -Value get-gitstatus
+Set-Alias -Name ls -Value get-dirlist -option AllScope -Force
+function get-gitstatus { git status }
+function get-dirlist { lsd -a1 }
+
+# Print a welcome message
+function printWelcome {
+  Write-Host
+  "             _    _ _____ _     _____ ________  ___ _____
+            | |  | |  ___| |   /  __ \  _  |  \/  ||  ___| 
+            | |  | | |__ | |   | /  \/ | | | .  . || |__ 
+            | |/\| |  __|| |   | |   | | | | |\/| ||  __| 
+            \  /\  / |___| |___| \__/\ \_/ / |  | || |___ 
+             \/  \/\____/\_____/\____/\___/\_|  |_/\____/
+
+                      ________ _______ ________
+                      ___  __ \___    |___  __/
+                      __  /_/ /__  /| |__  /
+                      _  ____/ _  ___ |_  /
+                      /_/      /_/  |_|/_/
+  "
+}
+printWelcome
+
+# Test if running as administrator
+# http://serverfault.com/questions/95431
+function Test-Administrator {
+  $user = [Security.Principal.WindowsIdentity]::GetCurrent();
+  (New-Object Security.Principal.WindowsPrincipal $user).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+}
+
+# Customize the prompt
+# https://github.com/dahlbyk/posh-git/wiki/Customizing-Your-PowerShell-Prompt
+function prompt {
+  # Write new line
+  Write-Host
+  $origLastExitCode = $LastExitCode
+  
+  # Test if admin
+  if (Test-Administrator) {
+    # if elevated
+    Write-Host "(Elevated) " -NoNewline -ForegroundColor White
+  }
+  
+  # write username@computername
+  Write-Host "$env:USERNAME@" -NoNewline -ForegroundColor DarkYellow
+  Write-Host "$env:COMPUTERNAME" -NoNewline -ForegroundColor Magenta
+  Write-Host " : " -NoNewline -ForegroundColor DarkGray
+
+  # Write current path
+  $curPath = $ExecutionContext.SessionState.Path.CurrentLocation.Path
+  if ($curPath.ToLower().StartsWith($Home.ToLower())) {
+    $curPath = "~" + $curPath.SubString($Home.Length)
+  }
+
+  # Write date & time
+  Write-Host $curPath -NoNewline -ForegroundColor Blue
+  Write-Host " : " -NoNewline -ForegroundColor DarkGray
+  Write-Host (Get-Date -Format G) -NoNewline -ForegroundColor DarkMagenta
+  Write-Host " :" -NoNewline -ForegroundColor DarkGray
+  
+  # Write posh-git status
+  Write-VcsStatus
+
+  $LastExitCode = $origLastExitCode
+  "`n$('>' * ($nestedPromptLevel + 1)) "
+}
+
+# Chocolatey profile
+#$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+#if (Test-Path($ChocolateyProfile)) { 
+#  Import-Module "$ChocolateyProfile" 
+#}
+
+# EOF
