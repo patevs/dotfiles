b0892f84fb6d318427968bd49ab1cb8362998716
PatEvs <ppevans11@gmail.com>
2019-07-11 20:12:49 +1200

    ✨  updates to npm/install.cmd

--- installs/npm/install.cmd
+++ installs/npm/install.cmd
@@ -1,38 +1,52 @@
-
+@ECHO OFF
 REM Script to install global NPM modules
 REM Requires Node.js and NPM to be installed
 
-@ECHO OFF
-ECHO Installing NPM global modules...
+cls
+ECHO. & ECHO [92m Installing NPM Global Modules... [0m & ECHO.
 
 REM Linting and style check
-npm install --global eslint
-npm install --global jshint
+
+call npm install --global eslint
+call npm install --global jshint
 
 REM Git integrations
-npm install --global g3l
-npm install --global git-open
-npm install --global gitmoji-cli
+
+call npm install --global g3l
+call npm install --global git-open
+call npm install --global gitmoji-cli
 
 REM Utilities
-npm install --global fkill-cli
-npm install --global ntl
-npm install --global open-cli
-npm install --global spaceholder
-npm install --global trash-cli
-npm install --global wifi-password-cli
+
+call npm install --global fkill-cli
+call npm install --global ntl
+call npm install --global open-cli
+call npm install --global spaceholder
+call npm install --global trash-cli
+call npm install --global wifi-password-cli
 
 REM Yeoman and generators
-npm install --global yo
-npm install --global generator-nm
+
+call npm install --global yo
+call npm install --global generator-nm
 
 REM Package Managers
-npm install --global yarn
-npm install --global pnpm
 
-REM Update pnpm
-pnpm install --global pnpm
+call npm install --global yarn
+call npm install --global pnpm
 
-ECHO DONE!
+REM Upgrade pnpm
+
+call pnpm install --global pnpm
+
+ECHO. & ECHO [92m Finished Installing NPM Global Modules! [0m & ECHO.
+
+REM Print NPM global installs
+
+ECHO [96m NPM Global Installs: [0m & ECHO.
+
+call npm list --global --depth=0
+
+ECHO [92m Done! [0m & ECHO.
 
 GOTO :EOF
