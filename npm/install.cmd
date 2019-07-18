3e997665226d4677709c96f48e78362781b330cf
PatEvs <ppevans11@gmail.com>
2019-07-18 17:48:55 +1200

    ✨  Updates to install.cmd

--- npm/install.cmd
+++ npm/install.cmd
@@ -7,14 +7,14 @@ ECHO. & ECHO [92m Installing NPM Global Modules... [0m & ECHO.
 
 REM Linting and style check
 
-ECHO [96m Installing linting and style check modules: [0m & ECHO.
+ECHO. & ECHO [96m Installing linting and style check modules: [0m & ECHO.
 
 CALL npm install --global eslint
 CALL npm install --global jshint
 
 REM Git integrations
 
-ECHO [96m Installing Git integration modules: [0m & ECHO.
+ECHO. & ECHO [96m Installing Git integration modules: [0m & ECHO.
 
 CALL npm install --global g3l
 CALL npm install --global git-open
@@ -22,7 +22,7 @@ CALL npm install --global gitmoji-cli
 
 REM Utilities
 
-ECHO [96m Installing utility modules: [0m & ECHO.
+ECHO. & ECHO [96m Installing utility modules: [0m & ECHO.
 
 CALL npm install --global fkill-cli
 CALL npm install --global ntl
@@ -33,25 +33,25 @@ CALL npm install --global wifi-password-cli
 
 REM Yeoman and generators
 
-ECHO [96m Installing Yeoman and generators modules: [0m & ECHO.
+ECHO. & ECHO [96m Installing Yeoman and generators modules: [0m & ECHO.
 
 CALL npm install --global yo
 CALL npm install --global generator-nm
 
 REM Package Managers
 
-ECHO [96m Installing Package Managers: [0m & ECHO.
+ECHO. & ECHO [96m Installing Package Managers: [0m & ECHO.
 
 CALL npm install --global yarn
 CALL npm install --global pnpm
 
 REM Upgrade pnpm
 
-ECHO [96m Upgrading PNPM: [0m & ECHO.
+ECHO. & ECHO [96m Upgrading PNPM: [0m & ECHO.
 
 CALL pnpm install --global pnpm
 
-ECHO. & ECHO [92m Finished Installing NPM Global Modules! [0m & ECHO.
+ECHO [92m Finished Installing NPM Global Modules! [0m & ECHO.
 
 REM Print NPM global installs
 
