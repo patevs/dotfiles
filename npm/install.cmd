e5f2f4212248225a74a799abfb7db7a770a81cde
PatEvs <ppevans11@gmail.com>
2019-07-19 12:11:29 +1200

    ✨  Updates to npm/install.cmd

--- npm/install.cmd
+++ npm/install.cmd
@@ -2,19 +2,36 @@
 REM Script to install global NPM modules
 REM Requires Node.js and NPM to be installed
 
+REM Verify Node.js and NPM are installed
+
 CLS
-ECHO. & ECHO [92m Installing NPM Global Modules... [0m & ECHO.
+ECHO. & ECHO [92m Verifying NodeJS and NPM Installations... [0m 
+REM & ECHO.
+
+ECHO. & ECHO [96m NodeJS Installation: [0m & ECHO.
+
+WHERE node
+IF %ERRORLEVEL% NEQ 0 ECHO node installation wasn't found... exiting! & GOTO :EOF
+
+ECHO. & ECHO [96m NPM Installation: [0m & ECHO.
+
+WHERE npm
+IF %ERRORLEVEL% NEQ 0 ECHO npm installation wasn't found... exiting! & GOTO :EOF
+
+REM Begin install script
+
+ECHO. & ECHO [92m Requirements Satisfied! Installing NPM Global Modules... [0m & ECHO.
 
 REM Linting and style check
 
-ECHO [96m Installing linting and style check modules: [0m & ECHO.
+ECHO [96m Installing Linting and Style Check Modules: [0m & ECHO.
 
 CALL npm install --global eslint
 CALL npm install --global jshint
 
 REM Git integrations
 
-ECHO. & ECHO [96m Installing Git integration modules: [0m & ECHO.
+ECHO. & ECHO [96m Installing Git Integration Modules: [0m & ECHO.
 
 CALL npm install --global g3l
 CALL npm install --global git-open
@@ -22,7 +39,7 @@ CALL npm install --global gitmoji-cli
 
 REM Utilities
 
-ECHO. & ECHO [96m Installing utility modules: [0m & ECHO.
+ECHO. & ECHO [96m Installing Utility Modules: [0m & ECHO.
 
 CALL npm install --global fkill-cli
 CALL npm install --global ntl
@@ -33,7 +50,7 @@ CALL npm install --global wifi-password-cli
 
 REM Yeoman and generators
 
-ECHO. & ECHO [96m Installing Yeoman and generators modules: [0m & ECHO.
+ECHO. & ECHO [96m Installing Yeoman and Generators: [0m & ECHO.
 
 CALL npm install --global yo
 CALL npm install --global generator-nm
