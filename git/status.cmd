1c04465dc2abc197dddb1041bed75e32d142a637
PatEvs <ppevans11@gmail.com>
2019-07-23 05:05:18 +1200

    ✨  Updates to git/status.cmd

--- git/status.cmd
+++ git/status.cmd
@@ -14,10 +14,20 @@ IF %ERRORLEVEL% NEQ 0 ECHO git installation wasn't found... exiting! & GOTO :EOF
 
 REM Begin check status script
 
-ECHO. & ECHO [92m Requirements Satisfied! Checking Git Status of all Directories... [0m & ECHO.
+ECHO. & ECHO [92m Requirements Satisfied! Starting Check Git Status Script... [0m & ECHO.
 
-REM :: CALL dir
+ECHO [96m Moving into Git Projects Directory: [0m & ECHO.
 
-ECHO [92m Done! [0m & ECHO.
+cd %UserProfile%\Desktop\git
+
+REM Echo current directory
+SET CURRENTDIR="%cd%"
+ECHO %CURRENTDIR%
+
+ECHO. & ECHO [96m Listing Directory Contents: [0m & ECHO.
+
+CALL dir /a:d /b
+
+ECHO. & ECHO [92m Done! [0m & ECHO.
 
 GOTO :EOF
