1fb5c598d906d3862945b17598402688d71d9a62
PatEvs <ppevans11@gmail.com>
2019-08-22 14:23:01 +1200

    ✨  Updates to npm/install.cmd

--- npm/install.cmd
+++ npm/install.cmd
@@ -3,18 +3,18 @@ REM Script to install useful NPM global modules.
 REM Requires Node.js and NPM to be installed.
 
 CLS
-ECHO. & ECHO [4mNPM Global Modules Install Script[0m
+ECHO. & ECHO [100;4mNPM Global Modules Install Script[0m
 
 REM Verify Node.js and NPM are installed & can be found
 
 ECHO. & ECHO [92m Verifying NodeJS and NPM Installations... [0m
 
-ECHO. & ECHO [96m NodeJS Installation: [0m & ECHO.
+ECHO. & ECHO  [45m NodeJS Installation: [0m & ECHO.
 
 WHERE node
 IF %ERRORLEVEL% NEQ 0 ECHO. & ECHO [91m NodeJS installation could not be found... exiting! [0m & GOTO :EOF
 
-ECHO. & ECHO [96m NPM Installation: [0m & ECHO.
+ECHO. & ECHO  [45m NPM Installation: [0m & ECHO.
 
 WHERE npm
 IF %ERRORLEVEL% NEQ 0 ECHO. & ECHO [91m npm installation could not be found... exiting! [0m & GOTO :EOF
@@ -25,14 +25,14 @@ ECHO. & ECHO [92m All Requirements Satisfied! Installing NPM Global Modules... 
 
 REM Linting and Style Check
 
-ECHO [96m Installing Linting and Style Check Modules: [0m & ECHO.
+ECHO  [44m Installing Linting and Style Check Modules: [0m & ECHO.
 
 CALL npm install --global eslint
 REM :: CALL npm install --global jshint
 
 REM Git Integrations and Utilities
 
-ECHO. & ECHO [96m Installing Git Integration and Utility Modules: [0m & ECHO.
+ECHO. & ECHO  [44m Installing Git Integration and Utility Modules: [0m & ECHO.
 
 CALL npm install --global g3l
 CALL npm install --global git-open
@@ -41,7 +41,7 @@ CALL npm install --global add-gitignore
 
 REM NodeJS Utilities
 
-ECHO. & ECHO [96m Installing NodeJS Utility Modules: [0m & ECHO.
+ECHO. & ECHO  [44m Installing NodeJS Utility Modules: [0m & ECHO.
 
 CALL npm install --global ntl
 CALL npm install --global david
@@ -60,7 +60,7 @@ REM :: CALL npm install --global ndb
 
 REM TypeScript and Utilities
 
-REM :: ECHO. & ECHO [96m Installing TypeScript and Utility Modules: [0m & ECHO.
+REM :: ECHO. & ECHO  [44m Installing TypeScript and Utility Modules: [0m & ECHO.
 
 REM :: CALL npm install --global typescript
 REM :: CALL npm install --global typesync
@@ -69,7 +69,7 @@ REM :: CALL npm install --global typescript-starter
 
 REM Development Utilities
 
-ECHO. & ECHO [96m Installing Development Utility Modules: [0m & ECHO.
+ECHO. & ECHO  [44m Installing Development Utility Modules: [0m & ECHO.
 
 CALL npm install --global strapi@beta
 REM :: font-awesome-svg-png requires rsvg-convert to be on $PATH
@@ -82,7 +82,7 @@ REM :: CALL npm install --global jsome
 
 REM System Utilities
 
-ECHO. & ECHO [96m Installing System Utility Modules: [0m & ECHO.
+ECHO. & ECHO  [44m Installing System Utility Modules: [0m & ECHO.
 
 CALL npm install --global open-cli
 CALL npm install --global trash-cli
@@ -93,7 +93,7 @@ REM :: CALL npm install --global fkill-cli
 
 REM Yeoman and generators
 
-ECHO. & ECHO [96m Installing Yeoman and Generators: [0m & ECHO.
+ECHO. & ECHO  [44m Installing Yeoman and Generators: [0m & ECHO.
 
 REM :: CALL npm install --global yo
 REM :: CALL npm install --global generator-nm
@@ -103,30 +103,30 @@ REM :: CALL npm install --global generator-typescript-starter
 
 REM Alternative Package Managers
 
-ECHO. & ECHO [96m Installing Alternative Package Managers: [0m & ECHO.
+ECHO. & ECHO  [44m Installing Alternative Package Managers: [0m & ECHO.
 
 CALL npm install --global yarn
 CALL npm install --global pnpm
 
 REM Upgrade pnpm
 
-ECHO. & ECHO [96m Upgrading PNPM: [0m & ECHO.
+ECHO. & ECHO  [44m Upgrading PNPM: [0m & ECHO.
 CALL pnpm install --global pnpm
 
 ECHO. & ECHO [92m Finished Installing NPM Global Modules! [0m & ECHO.
 
 REM Print NPM global installs
 
-ECHO [96m Listing NPM Global Installs: [0m & ECHO.
+ECHO  [44m Listing NPM Global Installs: [0m & ECHO.
 
 CALL npm list --global --depth=0
 
 REM Check for outdated modules
 
-ECHO [96m Checking for Outdated Modules: [0m & ECHO.
+ECHO  [44m Checking for Outdated Modules: [0m & ECHO.
 
 CALL david -g
 
-ECHO [42m Done! [0m & ECHO.
+ECHO  [42m Done! [0m & ECHO.
 
 GOTO :EOF
