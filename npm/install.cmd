90b8bc67c64ab9b17990f3b3f9f28d04feac70b4
PatEvs <ppevans11@gmail.com>
2019-08-24 17:10:52 +1200

    ✨  Updates to npm/install.cmd

--- npm/install.cmd
+++ npm/install.cmd
@@ -74,6 +74,7 @@ ECHO. & ECHO  [44m Installing Development Utility Modules: [0m & ECHO.
 
 CALL npm install --global strapi@beta
 CALL npm install --global create-react-app
+CALL npm install --global imagemin-cli
 REM :: font-awesome-svg-png requires rsvg-convert to be on $PATH
 REM :: CALL npm install --global font-awesome-svg-png
 REM :: mobicon-cli depends on GraphicsMagick being installed
