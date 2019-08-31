b935213fe87e63adf8c236c74a09f7e276f1e835
PatEvs <ppevans11@gmail.com>
2019-08-31 16:36:34 +1200

    ✨  Updates to npm/install.cmd

--- npm/install.cmd
+++ npm/install.cmd
@@ -95,8 +95,8 @@ ECHO. & ECHO  [44m Installing System Utility Modules: [0m & ECHO.
 
 CALL npm install --global trash-cli
 CALL npm install --global empty-trash-cli
+CALL npm install --global wifi-password-cli
 REM :: CALL npm install --global open-cli
-REM :: CALL npm install --global wifi-password-cli
 REM :: CALL npm install --global @rafaelrinaldi/whereami
 REM :: CALL npm install --global fkill-cli
 
