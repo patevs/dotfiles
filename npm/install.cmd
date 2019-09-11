f40e19f1a14888146ab78c6b8f3bad1cf39e07b9
PatEvs <ppevans11@gmail.com>
2019-09-12 08:28:24 +1200

    ✨  Updates to npm/install.cmd

--- npm/install.cmd
+++ npm/install.cmd
@@ -103,11 +103,12 @@ REM :: CALL npm install --global fkill-cli
 
 REM Yeoman and generators
 
-ECHO. & ECHO  [44m Installing Yeoman and Generators: [0m & ECHO.
+REM :: ECHO. & ECHO  [44m Installing Yeoman and Generators: [0m & ECHO.
 
-CALL npm install --global yo
+REM :: CALL npm install --global yo
+
 REM HTML/Frontend
-CALL npm install --global generator-h5bp
+REM :: CALL npm install --global generator-h5bp
 REM :: CALL npm install --global generator-webapp
 
 REM JavaScript/Node
