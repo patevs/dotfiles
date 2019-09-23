d540816bdcd5f6e50eab41b25e70435effa77e82
PatEvs <ppevans11@gmail.com>
2019-09-23 17:28:42 +1200

    ✨  Updates to music/install.sh

--- installs/music/install.sh
+++ installs/music/install.sh
@@ -1,63 +1,62 @@
 #!/bin/bash
 
-# music.sh
+# music-install.sh
 
-# Script to install beets along with required dependencies
-# mpsyt and set configuration
-
+# Script to install mpsyt & beets along with required dependencies
+# TODO: set mpsyt configuration
 # Requires Python3 and pip to be installed
 
 # Define color codes
 
 RED='\033[0;31m'
 GREEN='\033[0;32m'
-BROWN='\033[0;33m'
-BLUE='\033[0;34m'
+# BROWN='\033[0;33m'
+# BLUE='\033[0;34m'
 PURPLE='\033[0;35m'
-CYAN='\033[0;36m'
-LIGHT_GRAY='\033[0;37m'
-LIGHT_RED='\033[1;31m'
+# CYAN='\033[0;36m'
+# LIGHT_GRAY='\033[0;37m'
+# LIGHT_RED='\033[1;31m'
 LIGHT_GREEN='\033[1;32m'
-YELLOW='\033[1;33m'
-LIGHT_BLUE='\033[1;34m'
-LIGHT_PURPLE='\033[1;35m'
+# YELLOW='\033[1;33m'
+# LIGHT_BLUE='\033[1;34m'
+# LIGHT_PURPLE='\033[1;35m'
 LIGHT_CYAN='\033[1;36m'
-WHITE='\033[1;37m'
+# WHITE='\033[1;37m'
 NC='\033[0m' # No Color
 
-clear
-
+# clear
 printf "\n ---- ${GREEN}music.sh${NC} ---- \n"
-
-# printf "\nRunning music.sh\n"
 printf "\n${LIGHT_CYAN}Checking Requirements...${NC}\n\n"
 
-# Python
-command -v python >/dev/null 2>&1 || { 
-  echo >&2 "I require python but it's not installed.  Aborting."; exit 1; 
+# Check Python
+command -v python >/dev/null 2>&1 || {
+  echo >&2 "${PURPLE}Python${NC} is required but it's not installed. ${RED}Aborting${NC}."
+  exit 1
 }
-printf " * ${LIGHT_BLUE}Python${NC} : "
+printf " * ${PURPLE}Python${NC} : "
 python -V
 
-# pip
-command -v pip >/dev/null 2>&1 || { 
-  echo >&2 "I require pip but it's not installed.  Aborting."; exit 1; 
+# Check Pip
+command -v pip >/dev/null 2>&1 || {
+  echo >&2 "${PURPLE}Pip${NC} is required but it's not installed. ${RED}Aborting${NC}."
+  exit 1
 }
-printf " * ${LIGHT_BLUE}PIP${NC}    : "
+printf " * ${PURPLE}Pip${NC}    : "
 pip -V
 
 # Start install
-printf "\n${GREEN}Requirements Satisified!${NC}\n\n"
-# printf "${PURPLE} Starting install...${NC}\n\n"
+printf "\n${LIGHT_GREEN}Requirements Satisified! Beginning Install...${NC}\n\n"
 
 # Install mps-youtube
-bash mpsyt/mpsyt-install.sh
+# bash mpsyt/mpsyt-install.sh
 # Install beets
-bash beets/beets-install.sh
+# bash beets/beets-install.sh
 
 # Print pip installs
 # printf "\n${LIGHT_CYAN}PIP List...${NC}\n\n"
 # printf "  * running ${PURPLE}pip list${NC}\n\n"
 # pip list
 
-printf "\n${GREEN}DONE!${NC}\n"
+printf "\n${GREEN}DONE!${NC}\n\n"
+
+# EOF #
