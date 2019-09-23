53a86ae128f3d2f4cd418fa453f3c5cdcd9628f1
PatEvs <ppevans11@gmail.com>
2019-09-23 17:40:48 +1200

    ✨  mpsyt/mpsyt-install.sh

--- installs/music/mpsyt/mpsyt-install.sh
+++ installs/music/mpsyt/mpsyt-install.sh
@@ -10,20 +10,8 @@
 
 # Define color codes
 
-# RED='\033[0;31m'
 GREEN='\033[0;32m'
-# BROWN='\033[0;33m'
-# BLUE='\033[0;34m'
-# PURPLE='\033[0;35m'
-# CYAN='\033[0;36m'
-# LIGHT_GRAY='\033[0;37m'
-# LIGHT_RED='\033[1;31m'
-# LIGHT_GREEN='\033[1;32m'
-# YELLOW='\033[1;33m'
-# LIGHT_BLUE='\033[1;34m'
-# LIGHT_PURPLE='\033[1;35m'
-# LIGHT_CYAN='\033[1;36m'
-# WHITE='\033[1;37m'
+LIGHT_GREEN='\033[1;32m'
 NC='\033[0m' # No Color
 
 printf "\n ---- ${GREEN}mpsyt-install.sh${NC} ---- \n\n"
@@ -31,12 +19,13 @@ printf "\n ---- ${GREEN}mpsyt-install.sh${NC} ---- \n\n"
 # Install dependencies
 python -m pip install colorama
 python -m pip install youtube_dl
+
 # Install mps-youtube
 # requires ffmpeg and mpv
 python -m pip install mps-youtube
 
 # TODO: configure mpsyt
 
-printf "\nmpsyt-install.sh ${GREEN}DONE!${NC}\n"
+printf "\nmpsyt-install.sh ${LIGHT_GREEN}DONE!${NC}\n"
 
 # EOF #
