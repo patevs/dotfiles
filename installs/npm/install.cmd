649759ef60de7541d5b3d0e6158b5830b68494b3
PatEvs <ppevans11@gmail.com>
2019-09-26 17:58:19 +1200

    ✔️  Added lighthouse to npm/install.cmd

--- installs/npm/install.cmd
+++ installs/npm/install.cmd
@@ -79,6 +79,7 @@ ECHO. & ECHO  [44m Installing Development Utility Modules: [0m & ECHO.
 
 CALL npm install --global imagemin-cli
 CALL npm install --global licensed
+CALL npm install --global lighthouse
 CALL npm install --global spaceholder
 CALL npm install --global tldr
 
