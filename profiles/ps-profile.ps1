3c4be536962511fafe02a0d8020141bf0d56b5f2
patevs <ppevans11@gmail.com>
2019-10-18 16:23:40 +1300

    ✨  ps-profile.ps1

--- profiles/ps-profile.ps1
+++ profiles/ps-profile.ps1
@@ -29,6 +29,32 @@ function print-path {
 	($Env:Path).Split(";")
 }
 
+# Test if running as administrator
+# http://serverfault.com/questions/95431
+function Test-Administrator {
+  $user = [Security.Principal.WindowsIdentity]::GetCurrent();
+  (New-Object Security.Principal.WindowsPrincipal $user).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+}
+
+# Print a welcome message
+function printWelcome {
+  Write-Host
+  "             _    _ _____ _     _____ ________  ___ _____
+            | |  | |  ___| |   /  __ \  _  |  \/  ||  ___|
+            | |  | | |__ | |   | /  \/ | | | .  . || |__
+            | |/\| |  __|| |   | |   | | | | |\/| ||  __|
+            \  /\  / |___| |___| \__/\ \_/ / |  | || |___
+             \/  \/\____/\_____/\____/\___/\_|  |_/\____/
+
+                      ________ _______ ________
+                      ___  __ \___    |___  __/
+                      __  /_/ /__  /| |__  /
+                      _  ____/ _  ___ |_  /
+                      /_/      /_/  |_|/_/
+  "
+}
+printWelcome
+
 ######################
 # Unix-like Commands #
 ######################
@@ -121,6 +147,72 @@ Set-Alias -Name dev -Value moveDev
 # Set-Alias -Name gs -Value getGitStatus
 # TODO: Check for presence of g3l command
 # function getGitStatus { git status }
+
+# --- #
+# NPM #
+# --- #
+
+# Set-Alias -Name npl -Value getNpmLocals
+# Set-Alias -Name nplg -Value getNpmGlobals
+# function getNpmLocals { npm list --depth=0 }
+# function getNpmGlobals { npm list --global --depth=0 }
+
+# ---------- #
+# Chocolatey #
+# ---------- #
+
+# Set-Alias -Name cll -Value getChocoInstalls
+# Set-Alias -Name clo -Value getChocoOutdated
+# function getChocoInstalls { choco list -l }
+# function getChocoOutdated { choco upgrade all --noop }
+
+# Chocolatey profile
+# $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+# if (Test-Path($ChocolateyProfile)) {
+#   Import-Module "$ChocolateyProfile"
+# }
+
+####################
+# Customize Prompt #
+####################
+
+# https://github.com/dahlbyk/posh-git/wiki/Customizing-Your-PowerShell-Prompt
+function prompt {
+  # Write new line
+  Write-Host
+  $origLastExitCode = $LastExitCode
+
+  # Test if admin
+  if (Test-Administrator) {
+    # if elevated
+    Write-Host "(Elevated) " -NoNewline -ForegroundColor White
+  }
+
+  # write username@computername
+  Write-Host "$env:USERNAME@" -NoNewline -ForegroundColor DarkYellow
+  Write-Host "$env:COMPUTERNAME" -NoNewline -ForegroundColor Magenta
+  Write-Host " : " -NoNewline -ForegroundColor DarkGray
+
+  # Write current path
+  $curPath = $ExecutionContext.SessionState.Path.CurrentLocation.Path
+  if ($curPath.ToLower().StartsWith($Home.ToLower())) {
+    $curPath = "~" + $curPath.SubString($Home.Length)
+  }
+
+  # Write current path
+  Write-Host $curPath -NoNewline -ForegroundColor Blue
+  Write-Host " :" -NoNewline -ForegroundColor DarkGray
+
+  # Write date & time
+  # Write-Host (Get-Date -Format G) -NoNewline -ForegroundColor DarkMagenta
+  # Write-Host " :" -NoNewline -ForegroundColor DarkGray
+
+  # Write posh-git status
+  # Write-VcsStatus
+
+  $LastExitCode = $origLastExitCode
+  "`n$('>' * ($nestedPromptLevel + 1)) "
+}
 
 
 # EOF #
