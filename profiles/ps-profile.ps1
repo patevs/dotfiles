bc8af7873ea08af69afb51fac7ec97ee2acf06c0
patevs <ppevans11@gmail.com>
2019-10-18 16:19:11 +1300

    ✨  Updates to ps-profile.ps1

--- profiles/ps-profile.ps1
+++ profiles/ps-profile.ps1
@@ -12,10 +12,10 @@ $MaximumHistoryCount = 10000
 $PSDefaultParameterValues["Out-File:Encoding"]="utf8"
 
 # Import Posh-Git module
-Import-Module -Name posh-git
+# Import-Module -Name posh-git
 
 # Start SSH agent
-Start-SshAgent
+# Start-SshAgent
 
 ####################
 # Helper Functions #
@@ -56,54 +56,6 @@ function sudo {
 	$psi.Verb = "runas";
 	$psi.WorkingDirectory = get-location;
 	[System.Diagnostics.Process]::Start($psi) >> $null
-}
-
-# https://gist.github.com/aroben/5542538
-function pstree {
-	$ProcessesById = @{}
-	foreach ($Process in (Get-WMIObject -Class Win32_Process)) {
-		$ProcessesById[$Process.ProcessId] = $Process
-	}
-
-	$ProcessesWithoutParents = @()
-	$ProcessesByParent = @{}
-	foreach ($Pair in $ProcessesById.GetEnumerator()) {
-		$Process = $Pair.Value
-
-		if (($Process.ParentProcessId -eq 0) -or !$ProcessesById.ContainsKey($Process.ParentProcessId)) {
-			$ProcessesWithoutParents += $Process
-			continue
-		}
-
-		if (!$ProcessesByParent.ContainsKey($Process.ParentProcessId)) {
-			$ProcessesByParent[$Process.ParentProcessId] = @()
-		}
-		$Siblings = $ProcessesByParent[$Process.ParentProcessId]
-		$Siblings += $Process
-		$ProcessesByParent[$Process.ParentProcessId] = $Siblings
-	}
-
-	function Show-ProcessTree([UInt32]$ProcessId, $IndentLevel) {
-		$Process = $ProcessesById[$ProcessId]
-		$Indent = " " * $IndentLevel
-		if ($Process.CommandLine) {
-			$Description = $Process.CommandLine
-		} else {
-			$Description = $Process.Caption
-		}
-
-		Write-Output ("{0,6}{1} {2}" -f $Process.ProcessId, $Indent, $Description)
-		foreach ($Child in ($ProcessesByParent[$ProcessId] | Sort-Object CreationDate)) {
-			Show-ProcessTree $Child.ProcessId ($IndentLevel + 4)
-		}
-	}
-
-	Write-Output ("{0,6} {1}" -f "PID", "Command Line")
-	Write-Output ("{0,6} {1}" -f "---", "------------")
-
-	foreach ($Process in ($ProcessesWithoutParents | Sort-Object CreationDate)) {
-		Show-ProcessTree $Process.ProcessId 0
-	}
 }
 
 ###########
@@ -162,13 +114,13 @@ Set-Alias -Name dev -Value moveDev
 ###############
 
 # Use github client
-Set-Alias -Name git -Value getGithubClient
-function getGithubClient { hub $args }
+# Set-Alias -Name git -Value getGithubClient
+# function getGithubClient { hub $args }
 
 # Print Git status
-Set-Alias -Name gs -Value getGitStatus
+# Set-Alias -Name gs -Value getGitStatus
 # TODO: Check for presence of g3l command
-function getGitStatus { git status }
+# function getGitStatus { git status }
 
 
 # EOF #
