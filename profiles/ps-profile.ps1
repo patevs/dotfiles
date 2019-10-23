27cc0ae855eed16ebce6c16f995902ad77ad30de
PatEvs <ppevans11@gmail.com>
2019-10-23 22:49:18 +1300

    ✨  ps-profile.ps1

--- profiles/ps-profile.ps1
+++ profiles/ps-profile.ps1
@@ -5,6 +5,11 @@
 #          @author patevs          #
 # -------------------------------- #
 
+# So we can launch pwsh in subshells if we need
+# Add-PathVariable "${env:ProgramFiles}\PowerShell\6-preview"
+
+# $profileDir = $PSScriptRoot;
+
 # Increase history
 $MaximumHistoryCount = 10000
 
@@ -64,6 +69,20 @@ function Test-Administrator {
   $user = [Security.Principal.WindowsIdentity]::GetCurrent();
   (New-Object Security.Principal.WindowsPrincipal $user).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
 }
+
+# https://blogs.technet.microsoft.com/heyscriptingguy/2012/12/30/powertip-change-the-powershell-console-title
+# function set-title([string]$newtitle) {
+# 	$host.ui.RawUI.WindowTitle = $newtitle + ' – ' + $host.ui.RawUI.WindowTitle
+# }
+
+# From http://stackoverflow.com/questions/7330187/how-to-find-the-windows-version-from-the-powershell-command-line
+# function get-windows-build {
+# 	[Environment]::OSVersion
+# }
+
+# function get-serial-number {
+#   Get-CimInstance -ClassName Win32_Bios | select-object serialnumber
+# }
 
 # Print a welcome message
 function printWelcome {
@@ -351,6 +370,17 @@ function prompt {
   $LastExitCode = $origLastExitCode
   "`n$('>' * ($nestedPromptLevel + 1)) "
 }
+
+# --------------------------------------------------------------------------- #
+
+# foreach ( $includeFile in ("aws", "defaults", "openssl", "aws", "unix", "development", "node") ) {
+# 	Unblock-File $profileDir\$includeFile.ps1
+# . "$profileDir\$includeFile.ps1"
+# }
+
+# set-location '~/Code'
+
+write-output 'Pats profile loaded.'
 
 
 # EOF #
