f5b2f944a5ac1623dea850c5a224e55aec3b731a
PatEvs <ppevans11@gmail.com>
2019-10-24 21:44:40 +1300

    ✨  profiles/install.cmd

--- profiles/install.cmd
+++ profiles/install.cmd
@@ -1,1 +1,13 @@
+@ECHO OFF
+REM Script to setup powershell profile 
+REM Copyies ps-profile.ps1 to $PROFILE (~\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1).
+
+:: CLS
+ECHO. & ECHO [100;4mPowerShell Profile Install Script[0m
+
+CALL cp ps-profile.ps1 $PROFILE
+
+ECHO. & ECHO  [42m Done! [0m
+
+GOTO :EOF
 
