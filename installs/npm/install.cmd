eb5afdd29764bf80b65270a0c4d9be1da0a502db
PatEvs <ppevans11@gmail.com>
2019-10-27 17:39:01 +1300

    ✨  npm/install.cmd

--- installs/npm/install.cmd
+++ installs/npm/install.cmd
@@ -132,41 +132,41 @@ ECHO. & ECHO  [44m Installing Development Utility Modules: [0m & ECHO.
 
 :: # man pages and cheat sheets
 :: # https://github.com/tldr-pages/tldr
-REM # https://github.com/tldr-pages/tldr-node-client
-REM :: CALL npm install --global tldr
-REM # Image minifier
-REM # https://github.com/imagemin/imagemin-cli
-REM :: CALL npm install --global imagemin-cli
+:: # https://github.com/tldr-pages/tldr-node-client
+:: CALL npm install --global tldr
+:: # Image minifier
+:: # https://github.com/imagemin/imagemin-cli
+:: CALL npm install --global imagemin-cli
 :: # License generator
 :: # https://github.com/plibither8/licensed
 CALL npm install --global licensed
-REM # Google's website performance test
-REM # https://github.com/GoogleChrome/lighthouse/
+:: # Google's website performance test
+:: # https://github.com/GoogleChrome/lighthouse/
 :: CALL npm install --global lighthouse
-REM # Progressive web metrics
-REM # https://github.com/paulirish/pwmetrics
+:: # Progressive web metrics
+:: # https://github.com/paulirish/pwmetrics
 :: CALL npm install --global pwmetrics
-REM # Placeholder image generator
-REM # https://github.com/ecrmnn/spaceholder
-REM :: CALL npm install --global spaceholder
-REM # SVG Optimizer
-REM # https://github.com/svg/svgo
-REM :: CALL npm install --global svgo
-REM # https://github.com/facebook/create-react-app
-REM :: CALL npm install --global create-react-app
-REM # https://github.com/encharm/Font-Awesome-SVG-PNG
-REM :: NOTE: font-awesome-svg-png requires rsvg-convert to be on $PATH
-REM :: CALL npm install --global font-awesome-svg-png
-REM # https://github.com/Javascipt/Jsome
-REM :: CALL npm install --global jsome
-REM # https://github.com/WeiChiaChang/stacks-cli
-REM :: CALL npm install --global stacks-cli
-REM # https://github.com/strapi/strapi
-REM :: CALL npm install --global strapi@beta
-REM # https://github.com/kevva/viewport-list-cli
-REM :: CALL npm install --global viewport-list-cli
+:: # Placeholder image generator
+:: # https://github.com/ecrmnn/spaceholder
+:: CALL npm install --global spaceholder
+:: # SVG Optimizer
+:: # https://github.com/svg/svgo
+:: CALL npm install --global svgo
+:: # https://github.com/facebook/create-react-app
+:: CALL npm install --global create-react-app
+:: # https://github.com/encharm/Font-Awesome-SVG-PNG
+:: # NOTE: font-awesome-svg-png requires rsvg-convert to be on $PATH
+:: CALL npm install --global font-awesome-svg-png
+:: # https://github.com/Javascipt/Jsome
+:: CALL npm install --global jsome
+:: # https://github.com/WeiChiaChang/stacks-cli
+:: CALL npm install --global stacks-cli
+:: # https://github.com/strapi/strapi
+:: CALL npm install --global strapi@beta
+:: # https://github.com/kevva/viewport-list-cli
+:: CALL npm install --global viewport-list-cli
 
-REM System Utilities
+:: System Utilities
 ECHO. & ECHO  [44m Installing System Utility Modules: [0m & ECHO.
 
 REM # https://github.com/sindresorhus/empty-trash-cli
