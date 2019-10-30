ba2867b972a25a14502db89911a75b4d8891f93f
PatEvs <ppevans11@gmail.com>
2019-10-30 15:29:57 +1300

    ✨  alacritty/install.cmd

--- config/alacritty/install.cmd
+++ config/alacritty/install.cmd
@@ -6,12 +6,15 @@
 
 :: Batch script for easily setting up my alacritty terminal configuration
 :: The script copies `alacritty.yml` to ~\APPDATA\Roaming\alacritty\alacritty.yml
+:: and then reloads alacritty.
 
 :: CLS
 ECHO. & ECHO  [100;4m Alacritty Terminal Configuration Script [0m & ECHO.
 
 :: Copy Alacritty configuration
 CALL xcopy /Y alacritty.yml C:\Users\Patrick\APPDATA\Roaming\alacritty\alacritty.yml
+:: Reload Alacritty
+CALL alacritty
 
 :: ECHO. & ECHO  [42m Done! [0m
 
