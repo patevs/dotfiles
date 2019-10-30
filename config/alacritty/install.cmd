d847618e2e46ebd90fcfcc3723faee0b3429c199
PatEvs <ppevans11@gmail.com>
2019-10-30 15:38:34 +1300

    ✨  alacritty/install.cmd

--- config/alacritty/install.cmd
+++ config/alacritty/install.cmd
@@ -14,8 +14,6 @@ ECHO. & ECHO  [100;4m Alacritty Terminal Configuration Script [0m & ECHO.
 CALL xcopy /Y alacritty.yml C:\Users\Patrick\APPDATA\Roaming\alacritty\alacritty.yml
 :: Reload Alacritty
 CALL alacritty
-:: Close previous alacritty instance
-:: CALL exit
 
 :: ECHO. & ECHO  [42m Done! [0m
 
