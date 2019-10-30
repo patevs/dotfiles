b252de58a1f5b931250ac17c87244b26bddba80a
PatEvs <ppevans11@gmail.com>
2019-10-30 15:21:50 +1300

    ✨  Updates to config/powershell/install.cmd

--- config/powershell/install.cmd
+++ config/powershell/install.cmd
@@ -1,17 +1,23 @@
 @ECHO OFF
-REM Script to setup powershell profile 
-REM Copyies ps-profile.ps1 to $PROFILE (~\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1).
+
+:: ----------------------------- ::
+:: config/PowerShell/install.cmd ::
+:: ----------------------------- ::
+
+:: Batch script for easily setting up my PowerShell profile
+:: The script copies `ps-profile.ps1` to $PROFILE (~\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1)
+:: and then reloads PowerShell to activate the profile.
 
 :: CLS
 ECHO. & ECHO  [100;4m PowerShell Profile Install Script [0m & ECHO.
 
-:: Copy powershell profile
+:: Copy PowerShell profile
 :: CALL cp ps-profile.ps1 $PROFILE
 CALL xcopy /Y ps-profile.ps1 C:\Users\Patrick\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1
-:: Reload powershell
-CALL powershell 
+:: Reload PowerShell
+CALL powershell
 
-ECHO. & ECHO  [42m Done! [0m
+:: ECHO. & ECHO  [42m Done! [0m
 
 GOTO :EOF
 
