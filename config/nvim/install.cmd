40bfff28027273b36035167b250ece8d3f861db4
PatEvs <ppevans11@gmail.com>
2019-11-06 16:39:38 +1300

    ✨  config/nvim/install.cmd

--- config/nvim/install.cmd
+++ config/nvim/install.cmd
@@ -4,14 +4,14 @@
 :: config/nvim/install.cmd ::
 :: ----------------------- ::
 
-:: Batch script for easily setting up my vim/neovim configuration
+:: Batch script for easily setting up my neovim configuration
 :: The script copies profile scripts to ~\APPDATA\Local\nvim\init.vim
 
 :: CLS
-ECHO. & ECHO  [100;4m Vim Profile Install Script [0m & ECHO.
+ECHO. & ECHO  [100;4m NeoVim Profile Install Script [0m & ECHO.
 
 :: Copy vim profile scripts
-CALL xcopy /Y init.vim C:\Users\Patrick\APPDATA\Local\nvim\init.vim
+CALL ECHO F | xcopy /Y /Q init.vim C:\Users\Patrick\APPDATA\Local\nvim\init.vim
 :: New line
 ECHO.
 
