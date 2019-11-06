fa792469878d7beba71dbe657490040662a61efe
PatEvs <ppevans11@gmail.com>
2019-11-06 16:34:16 +1300

    ✨  config/bash/install.cmd

---
+++ config/bash/install.cmd
@@ -0,0 +1,27 @@
+@ECHO OFF
+
+:: ----------------------- ::
+:: config/bash/install.cmd ::
+:: ----------------------- ::
+
+:: Batch script for easily setting up my bash configuration
+:: The script copies '.bash_aliases', '.bash_profile', '.bashrc', and '.profile'
+:: into my home directory.
+
+:: CLS
+ECHO. & ECHO  [100;4m Bash Configuration Script [0m & ECHO.
+
+:: Copy bash configuration files
+CALL xcopy /Y .bash_aliases C:\Users\Patrick\.bash_aliases
+CALL xcopy /Y .bash_profile C:\Users\Patrick\.bash_profile
+CALL xcopy /Y .bashrc C:\Users\Patrick\.bashrc
+CALL xcopy /Y .profile C:\Users\Patrick\.profile
+
+:: Reload bash
+:: CALL bash
+
+:: ECHO. & ECHO  [42m Done! [0m
+
+GOTO :EOF
+
+:: EOF ::
