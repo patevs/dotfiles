8d02d380887437533db819d7093bb644bacbcb66
PatEvs <ppevans11@gmail.com>
2019-11-06 19:29:13 +1300

    ✨  bash/install.cmd

--- config/bash/install.cmd
+++ config/bash/install.cmd
@@ -12,10 +12,10 @@
 ECHO. & ECHO  [100;4m Bash Configuration Script [0m & ECHO.
 
 :: Copy bash configuration files
-CALL ECHO F | xcopy /Y .bash_aliases C:\Users\Patrick\.bash_aliases
-CALL ECHO F | xcopy /Y .bash_profile C:\Users\Patrick\.bash_profile
-CALL ECHO F | xcopy /Y .bashrc C:\Users\Patrick\.bashrc
-CALL ECHO F | xcopy /Y .profile C:\Users\Patrick\.profile
+CALL ECHO F | xcopy /Y /Q .bash_aliases %USERPROFILE%\.bash_aliases
+CALL ECHO F | xcopy /Y /Q .bash_profile %USERPROFILE%\.bash_profile
+CALL ECHO F | xcopy /Y /Q .bashrc %USERPROFILE%\.bashrc
+CALL ECHO F | xcopy /Y /Q .profile %USERPROFILE%\.profile
 
 :: Reload bash
 :: CALL bash
