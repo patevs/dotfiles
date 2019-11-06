bb447553fd4e2265ee73208dbf5f62f6e7497348
PatEvs <ppevans11@gmail.com>
2019-11-06 15:31:03 +1300

    ✨  npm/setup.cmd

--- scripts/npm/setup.cmd
+++ scripts/npm/setup.cmd
@@ -13,7 +13,7 @@
 ::  https://stackoverflow.com/questions/51330735/npm-config-global-vs-local
 
 :: CLS
-ECHO. & ECHO [100;4mNPM Global Configuration Setup Script[0m
+ECHO. & ECHO  [100m [100;4mNPM Global Configuration Setup Script[0m[100m [0m
 
 :: Verify NPM is installed & can be found
 ECHO. & ECHO [92m Verifying NPM Installation... [0m
@@ -22,7 +22,8 @@ WHERE npm
 IF %ERRORLEVEL% NEQ 0 ECHO. & ECHO [91m npm installation could not be found... exiting! [0m & GOTO :EOF
 
 :: Begin NPM Global Configuration Setup
-ECHO. & ECHO [92m All Requirements Satisfied! Starting NPM Global Configuration Setup... [0m & ECHO.
+ECHO. & ECHO [92m All Requirements Satisfied! Starting NPM Global Configuration Setup... [0m
+:: & ECHO.
 
 ECHO. & ECHO  [44m Setting NPM Global Configuration [0m & ECHO.
 
@@ -35,11 +36,11 @@ CALL npm config set --global init.author.url "https://github.com/patevs"
 CALL npm config set --global init.license "MIT"
 
 :: Print NPM global configuration
-ECHO [44m NPM Global Configuration: [0m & ECHO.
+ECHO [42m NPM Global Configuration: [0m & ECHO.
 
 CALL npm config list --global
 
-ECHO. & ECHO  [42m Done! [0m
+ECHO. & ECHO  [42m Done! [0m & ECHO.
 
 GOTO :EOF
 
