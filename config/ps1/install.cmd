e1ba2429de41204fec30629514881e8775d0a7b9
PatEvs <ppevans11@gmail.com>
2020-03-23 18:59:20 +1300

    ✨  Updated ps1/install.cmd script

--- config/ps1/install.cmd
+++ config/ps1/install.cmd
@@ -14,11 +14,14 @@
 ECHO. & ECHO  [100m [100;4mPowerShell Profile Install Script[0m[100m [0m & ECHO.
 :: ECHO. & ECHO  [100;4m PowerShell Profile Install Script [0m & ECHO.
 
+:: TODO: Update script to dynamically find all config files
+
 :: Copy PowerShell profile scripts
 CALL ECHO F | xcopy /Y /Q Microsoft.PowerShell_profile.ps1 %USERPROFILE%\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1
 CALL ECHO F | xcopy /Y /Q defaults.ps1 %USERPROFILE%\Documents\WindowsPowerShell\defaults.ps1
 CALL ECHO F | xcopy /Y /Q unix.ps1 %USERPROFILE%\Documents\WindowsPowerShell\unix.ps1
 CALL ECHO F | xcopy /Y /Q development.ps1 %USERPROFILE%\Documents\WindowsPowerShell\development.ps1
+CALL ECHO F | xcopy /Y /Q completions\rustup.ps1 %USERPROFILE%\Documents\WindowsPowerShell\completions\rustup.ps1
 :: New line
 ECHO.
 :: Reload PowerShell
