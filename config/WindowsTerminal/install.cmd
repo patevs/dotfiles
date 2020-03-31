e037e09f9bed56bfeb475029b3334da069e2606d
PatEvs <ppevans11@gmail.com>
2020-03-31 15:45:57 +1300

    Updated WindowsTerminal/install.cmd script

--- config/WindowsTerminal/install.cmd
+++ config/WindowsTerminal/install.cmd
@@ -18,7 +18,8 @@ CALL ECHO F | xcopy /Y /Q profiles.json "%USERPROFILE%\APPDATA\Local\Microsoft\W
 ECHO.
 :: Reload WindowsTerminal
 :: CALL powershell -nologo
-CALL windowsterminal.exe
+:: CALL windowsterminal.exe
+CALL windowsterminal
 
 :: ECHO. & ECHO  [42m Done! [0m
 
