0c18bac7a29b17450e7afb4eae3b7c77b8cbe5e0
PatEvs <ppevans11@gmail.com>
2020-04-08 15:54:29 +1200

    ✨  Updated wt/install.ps1

--- wt/install.ps1
+++ wt/install.ps1
@@ -1,4 +1,80 @@
 <#
+.SYNOPSIS
+  Windows Terminal profile install script.
+
+.DESCRIPTION
+  Copies Windows Terminal profile.json to:
+
+    "%USERPROFILE%\AppData\Local\Microsoft\Windows Terminal\profiles.json"
+
+.NOTES
+  Version:        1.0
+  Author:         PatEvs (github.com/patevs)
+  Last Modified:  08/04/2020 - April 8th 2020
+
+.EXAMPLE
+  .\install.ps1
+#>
+
+#---------------------------------------------------------[Initialisations]--------------------------------------------------------
+
+#Set Error Action to Silently Continue
+# $ErrorActionPreference = "SilentlyContinue"
+
+#Dot Source required Function Libraries
+# . "C:\Scripts\Functions\Logging_Functions.ps1"
+
+#----------------------------------------------------------[Declarations]----------------------------------------------------------
+
+#Script Version
+# $sScriptVersion = "1.0"
+
+#Log File Info
+# $sLogPath = "C:\Windows\Temp"
+# $sLogName = "<script_name>.log"
+# $sLogFile = Join-Path -Path $sLogPath -ChildPath $sLogName
+
+#-----------------------------------------------------------[Functions]------------------------------------------------------------
+
+<#
+
+Function <FunctionName>{
+  Param()
+
+  Begin{
+    Log-Write -LogPath $sLogFile -LineValue "<description of what is going on>..."
+  }
+
+  Process{
+    Try{
+      <code goes here>
+    }
+
+    Catch{
+      Log-Error -LogPath $sLogFile -ErrorDesc $_.Exception -ExitGracefully $True
+      Break
+    }
+  }
+
+  End{
+    If($?){
+      Log-Write -LogPath $sLogFile -LineValue "Completed Successfully."
+      Log-Write -LogPath $sLogFile -LineValue " "
+    }
+  }
+}
+
+#>
+
+#-----------------------------------------------------------[Execution]------------------------------------------------------------
+
+#Log-Start -LogPath $sLogPath -LogName $sLogName -ScriptVersion $sScriptVersion
+#Script Execution goes here
+#Log-Finish -LogPath $sLogFile
+
+# EOF #
+
+<#
 $profileDir = Split-Path -parent $profile
 $componentDir = Join-Path $profileDir "components"
 
