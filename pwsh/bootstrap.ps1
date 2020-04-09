71e33a7bf73685aa605f78d0d2b04c8c5efb03ce
PatEvs <ppevans11@gmail.com>
2020-04-09 18:08:49 +1200

    ✨  Updated pwsh/bootstrap.ps1

--- pwsh/bootstrap.ps1
+++ pwsh/bootstrap.ps1
@@ -11,7 +11,7 @@ Write-Output "$componentDir"
 # New-Item $profileDir -ItemType Directory -Force -ErrorAction SilentlyContinue
 # New-Item $componentDir -ItemType Directory -Force -ErrorAction SilentlyContinue
 
-# Copy-Item -Path ./*.ps1 -Destination $profileDir -Exclude "bootstrap.ps1"
+Copy-Item -Path ./*.ps1 -Destination $profileDir -Exclude "bootstrap.ps1"
 # Copy-Item -Path ./components/** -Destination $componentDir -Include **
 # Copy-Item -Path ./home/** -Destination $home -Include **
 
