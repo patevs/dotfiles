b9d0263176b0ad787d5f3eb75a884ff5d562b7ef
PatEvs <ppevans11@gmail.com>
2020-04-11 17:36:26 +1200

    ✨  Updated ps1/components/yarn.ps1

--- ps1/components/yarn.ps1
+++ ps1/components/yarn.ps1
@@ -1,6 +1,6 @@
-# ------------------ #
+# ------------------- #
 # components/yarn.ps1 #
-# ------------------ #
+# ------------------- #
 
 if ((Get-Command yarn -ErrorAction SilentlyContinue) -and (Get-Module -ListAvailable yarn-completion -ErrorAction SilentlyContinue)) {
   Import-Module -Name yarn-completion
