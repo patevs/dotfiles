a77a056a80bd6bbc0cbcbe7fe6f9f71e8963654f
PatEvs <ppevans11@gmail.com>
2020-04-11 17:22:39 +1200

    ✨  Updated ps1/components/console.ps1

--- ps1/components/console.ps1
+++ ps1/components/console.ps1
@@ -9,10 +9,10 @@
 # http://serverfault.com/questions/95431
 function Test-Elevated {
   # Get the ID and security principal of the current user account
-  $myIdentity=[System.Security.Principal.WindowsIdentity]::GetCurrent()
-  $myPrincipal=new-object System.Security.Principal.WindowsPrincipal($myIdentity)
+  $userID = [System.Security.Principal.WindowsIdentity]::GetCurrent()
+  $userPrincipal = New-Object System.Security.Principal.WindowsPrincipal($userID)
   # Check to see if we are currently running "as Administrator"
-  return $myPrincipal.IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator)
+  return $userPrincipal.IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator)
 }
 
 # --------------------------------------------------------------------------------------------- #
