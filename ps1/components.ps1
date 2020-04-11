1e30484f7f014a3855ecccfd530d15e5f28acfc1
PatEvs <ppevans11@gmail.com>
2020-04-11 17:37:48 +1200

    ✨  Updated ps1/components.ps1

--- ps1/components.ps1
+++ ps1/components.ps1
@@ -7,7 +7,10 @@
 Push-Location (Join-Path (Split-Path -parent $profile) "components")
 
 # From within the ./components directory...
+. .\cargo.ps1
+. .\dotnet.ps1
 . .\git.ps1
+. .\npm.ps1
 . .\yarn.ps1
 
 Pop-Location
