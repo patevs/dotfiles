e44565f70fe06001c8288e64505aad9c5cd7ccf9
PatEvs <ppevans11@gmail.com>
2020-04-15 02:20:03 +1200

    ✨  Created pwsh/profile.ps1

---
+++ pwsh/profile.ps1
@@ -0,0 +1,15 @@
+# ----------- #
+# profile.ps1 #
+# ----------- #
+
+# Profile for PowerShell Core
+# ===========================
+
+Write-Output "profile.ps1"
+
+Push-Location (Split-Path -parent $profile)
+# "components","functions","aliases","exports" | Where-Object {Test-Path "$_.ps1"} | ForEach-Object -process {Invoke-Expression ". .\$_.ps1"}
+"components" | Where-Object {Test-Path "$_.ps1"} | ForEach-Object -process {Invoke-Expression ". .\$_.ps1"}
+Pop-Location
+
+# EOF #
