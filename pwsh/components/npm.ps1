90582f367367a619ac39c7f10f0e2b5ada26e019
PatEvs <ppevans11@gmail.com>
2020-04-15 02:27:27 +1200

    ✨  Created pwsh/components/npm.ps1

---
+++ pwsh/components/npm.ps1
@@ -0,0 +1,12 @@
+# ------------------ #
+# components/npm.ps1 #
+# ------------------ #
+
+Write-Output "components/npm.ps1"
+
+if ((Get-Command npm -ErrorAction SilentlyContinue) -and (Get-Module -ListAvailable npm-completion -ErrorAction SilentlyContinue)) {
+  # Import-Module -Name posh-npm
+  Import-Module -Name npm-completion
+}
+
+# EOF #
