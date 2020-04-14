9296bac5027b446621a2587ffe21edf338b52e2b
PatEvs <ppevans11@gmail.com>
2020-04-15 02:36:02 +1200

    ✨  Updated pwsh/profile.ps1

--- pwsh/profile.ps1
+++ pwsh/profile.ps1
@@ -9,7 +9,7 @@ Write-Output "profile.ps1"
 
 Push-Location (Split-Path -parent $profile)
 # "components","functions","aliases","exports" | Where-Object {Test-Path "$_.ps1"} | ForEach-Object -process {Invoke-Expression ". .\$_.ps1"}
-"components", "functions" | Where-Object {Test-Path "$_.ps1"} | ForEach-Object -process {Invoke-Expression ". .\$_.ps1"}
+"components", "functions", "aliases" | Where-Object {Test-Path "$_.ps1"} | ForEach-Object -process {Invoke-Expression ". .\$_.ps1"}
 Pop-Location
 
 # EOF #
