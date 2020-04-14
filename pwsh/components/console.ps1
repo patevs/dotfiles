a249070f9e1e844cd43bed10bf645b9bc064587c
PatEvs <ppevans11@gmail.com>
2020-04-14 20:10:48 +1200

    ✨  Updated pwsh/components/console.ps1

--- pwsh/components/console.ps1
+++ pwsh/components/console.ps1
@@ -69,13 +69,13 @@ if (Get-Module -ListAvailable PSReadLine -ErrorAction SilentlyContinue) {
 
 # Must be called 'prompt' to be used by pwsh
 # https://github.com/gummesson/kapow/blob/master/themes/bashlet.ps1
-# function prompt {
-#   $realLASTEXITCODE = $LASTEXITCODE
-#   Write-Host $(limit-HomeDirectory("$pwd")) -ForegroundColor Yellow -NoNewline
-#   Write-Host " $" -NoNewline
-#   $global:LASTEXITCODE = $realLASTEXITCODE
-#   Return " "
-# }
+function prompt {
+  $realLASTEXITCODE = $LASTEXITCODE
+  Write-Host $(limit-HomeDirectory("$pwd")) -ForegroundColor Yellow -NoNewline
+  Write-Host " $" -NoNewline
+  $global:LASTEXITCODE = $realLASTEXITCODE
+  Return " "
+}
 
 # https://github.com/dahlbyk/posh-git/wiki/Customizing-Your-PowerShell-Prompt
 <#
