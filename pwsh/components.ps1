66096a4df487f50c1b21b92d4e706f8277457d7a
PatEvs <ppevans11@gmail.com>
2020-04-15 02:24:03 +1200

    ✨  Updated pwsh/components.ps1

--- pwsh/components.ps1
+++ pwsh/components.ps1
@@ -11,7 +11,7 @@ Push-Location (Join-Path (Split-Path -parent $profile) "components")
 # From within the ./components directory...
 # . .\cargo.ps1
 # . .\dotnet.ps1
-# . .\git.ps1
+. .\git.ps1
 # . .\npm.ps1
 # . .\yarn.ps1
 
