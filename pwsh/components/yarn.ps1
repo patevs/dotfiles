036028f13405c9f32556af5872b7a0963ee0519d
PatEvs <ppevans11@gmail.com>
2020-04-15 02:28:12 +1200

    ✨  Created pwsh/components/yarn.ps1

---
+++ pwsh/components/yarn.ps1
@@ -0,0 +1,11 @@
+# ------------------- #
+# components/yarn.ps1 #
+# ------------------- #
+
+Write-Output "components/yarn.ps1"
+
+if ((Get-Command yarn -ErrorAction SilentlyContinue) -and (Get-Module -ListAvailable yarn-completion -ErrorAction SilentlyContinue)) {
+  Import-Module -Name yarn-completion
+}
+
+# EOF #
