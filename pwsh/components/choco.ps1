1b0869a8dbd244dac998d51a78901070765657bf
PatEvs <ppevans11@gmail.com>
2020-04-15 17:33:44 +1200

    ✨  Updated pwsh/components/choco.ps1

--- pwsh/components/choco.ps1
+++ pwsh/components/choco.ps1
@@ -4,12 +4,6 @@
 
 Write-Output "components/choco.ps1"
 
-<#
-if ((Get-Command choco -ErrorAction SilentlyContinue) -and (Get-Module -ListAvailable posh-cargo -ErrorAction SilentlyContinue)) {
-  Import-Module -Name posh-cargo
-}
-#>
-
 # Chocolatey Profile
 $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
 if (Test-Path($ChocolateyProfile)) {
