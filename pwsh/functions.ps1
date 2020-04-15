3830622a0c3017b396bd6df3b7a85b80cc0bb054
PatEvs <ppevans11@gmail.com>
2020-04-15 23:54:57 +1200

    :bulb: Comments in pwsh/functions.ps1

--- pwsh/functions.ps1
+++ pwsh/functions.ps1
@@ -82,6 +82,29 @@ function sudo() {
 }
 #>
 
+# From https://github.com/Pscx/Pscx
+# function sudo(){
+# 	Invoke-Elevated @args
+# }
+
+# function sudo {
+#	 $file, [string]$arguments = $args;
+#	 $psi = new-object System.Diagnostics.ProcessStartInfo $file;
+#  $psi.Arguments = $arguments;
+#  $psi.Verb = "runas";
+#  $psi.WorkingDirectory = get-location;
+#  [System.Diagnostics.Process]::Start($psi) >> $null
+# }
+
+# https://stackoverflow.com/questions/7690994/running-a-command-as-administrator-using-powershell
+# function sudo {
+  # Start-Process powershell -Verb runAs
+#   if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+#     Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs;
+#     exit
+#   }
+# }
+
 # ------------------------------------------------------------------------------------------------------- #
 
 # PowerShell Utility Functions
