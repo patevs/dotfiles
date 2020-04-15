f9d570f5df5992be39a6ed9ea9d9fbbf22984575
PatEvs <ppevans11@gmail.com>
2020-04-15 23:56:27 +1200

    :bulb: Formatting comments in pwsh/functions.ps1

--- pwsh/functions.ps1
+++ pwsh/functions.ps1
@@ -80,30 +80,31 @@ function sudo() {
     start-process $args[0] -ArgumentList $args[1..$args.Length] -verb "runAs"
   }
 }
-#>
 
 # From https://github.com/Pscx/Pscx
-# function sudo(){
-# 	Invoke-Elevated @args
-# }
+function sudo(){
+  Invoke-Elevated @args
+}
 
-# function sudo {
-#	 $file, [string]$arguments = $args;
-#	 $psi = new-object System.Diagnostics.ProcessStartInfo $file;
-#  $psi.Arguments = $arguments;
-#  $psi.Verb = "runas";
-#  $psi.WorkingDirectory = get-location;
-#  [System.Diagnostics.Process]::Start($psi) >> $null
-# }
+function sudo {
+  $file, [string]$arguments = $args;
+  $psi = new-object System.Diagnostics.ProcessStartInfo $file;
+  $psi.Arguments = $arguments;
+  $psi.Verb = "runas";
+  $psi.WorkingDirectory = get-location;
+  [System.Diagnostics.Process]::Start($psi) >> $null
+}
 
 # https://stackoverflow.com/questions/7690994/running-a-command-as-administrator-using-powershell
-# function sudo {
-  # Start-Process powershell -Verb runAs
-#   if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
-#     Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs;
-#     exit
-#   }
-# }
+function sudo {
+  Start-Process powershell -Verb runAs
+  if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+    Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs;
+    exit
+  }
+}
+#>
+
 
 # ------------------------------------------------------------------------------------------------------- #
 
