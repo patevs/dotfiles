567fc45e0d406438f7637e3c375327c71ff12b99
PatEvs <ppevans11@gmail.com>
2020-04-16 18:02:01 +1200

    :sparkles: Created pwsh/exports.ps1

---
+++ pwsh/exports.ps1
@@ -0,0 +1,12 @@
+# ----------- #
+# exports.ps1 #
+# ----------- #
+
+# Make vim the default editor
+# Set-Environment "EDITOR" "gvim --nofork"
+# Set-Environment "GIT_EDITOR" $Env:EDITOR
+
+# Disable the Progress Bar
+# $ProgressPreference='SilentlyContinue'
+
+# EOF #
