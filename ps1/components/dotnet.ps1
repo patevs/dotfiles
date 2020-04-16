a22e27aa0c13f4b1ccef74f2f5c1ba8e23499999
PatEvs <ppevans11@gmail.com>
2020-04-16 18:02:51 +1200

    :fire: Removed ps1/

--- ps1/components/dotnet.ps1
+++
@@ -1,9 +0,0 @@
-# --------------------- #
-# components/dotnet.ps1 #
-# --------------------- #
-
-if ((Get-Command dotnet -ErrorAction SilentlyContinue) -and (Get-Module -ListAvailable posh-dotnet -ErrorAction SilentlyContinue)) {
-  Import-Module -Name posh-dotnet
-}
-
-# EOF #
--- ps1/components/yarn.ps1
+++
@@ -1,9 +0,0 @@
-# ------------------- #
-# components/yarn.ps1 #
-# ------------------- #
-
-if ((Get-Command yarn -ErrorAction SilentlyContinue) -and (Get-Module -ListAvailable yarn-completion -ErrorAction SilentlyContinue)) {
-  Import-Module -Name yarn-completion
-}
-
-# EOF #
