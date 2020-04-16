322887e4abe37ce0908c8af7b5a96f996cfa89d9
PatEvs <ppevans11@gmail.com>
2020-04-16 15:26:30 +1200

    :fire: Removed old code from pwsh/aliases.ps1

--- pwsh/aliases.ps1
+++ pwsh/aliases.ps1
@@ -90,27 +90,7 @@ if (Get-Command lsd -ErrorAction SilentlyContinue | Test-Path) {
   ${function:lst} = { tree }
 }
 
-# Directory Listing: Use `ls.exe` if available
-<#
-if (Get-Command ls.exe -ErrorAction SilentlyContinue | Test-Path) {
-    rm alias:ls -ErrorAction SilentlyContinue
-    # Set `ls` to call `ls.exe` and always use --color
-    ${function:ls} = { ls.exe --color @args }
-    # List all files in long format
-    ${function:l} = { ls -lF @args }
-    # List all files in long format, including hidden files
-    ${function:la} = { ls -laF @args }
-    # List only directories
-    ${function:lsd} = { Get-ChildItem -Directory -Force @args }
-} else {
-    # List all files, including hidden files
-    ${function:la} = { ls -Force @args }
-    # List only directories
-    ${function:lsd} = { Get-ChildItem -Directory -Force @args }
-}
-#>
-
-# Print list of current directory contents
+# List directory contents
 Set-Alias -Name ls -Value ll -option AllScope -Force
 
 # Missing Bash aliases
