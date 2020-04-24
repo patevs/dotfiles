d50206454653a167393f3b22eca9e5617b4729b5
Patrick Evans <ppevans11@gmail.com>
2020-04-24 15:57:39 +1200

    :sparkles: Updated deps/components/powershell.ps1

--- deps/components/powershell.ps1
+++ deps/components/powershell.ps1
@@ -21,4 +21,24 @@
     * https://github.com/patevs/dotfiles/blob/master/deps/components/powershell.ps1
 #>
 
+# Test if running as administrator
+# http://serverfault.com/questions/95431
+function Test-Elevated {
+  # Get the ID and security principal of the current user account
+  $userID = [System.Security.Principal.WindowsIdentity]::GetCurrent()
+  $userPrincipal = New-Object System.Security.Principal.WindowsPrincipal($userID)
+  # Check to see if we are currently running "as Administrator"
+  return $userPrincipal.IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator)
+}
+
+# Check to see if we are currently running "as Administrator"
+if (!(Test-Elevated)) {
+  $newProcess = new-object System.Diagnostics.ProcessStartInfo "PowerShell";
+  $newProcess.Arguments = $myInvocation.MyCommand.Definition;
+  $newProcess.Verb = "runas";
+  [System.Diagnostics.Process]::Start($newProcess);
+
+  exit
+}
+
 # EOF #
