306d12802ea2e713a19b728ba86892db8c630554
Patrick Evans <ppevans11@gmail.com>
2020-04-24 15:56:14 +1200

    :bulb: Added comments to deps/components/powershell.ps1

---
+++ deps/components/powershell.ps1
@@ -0,0 +1,24 @@
+<#
+
+.SYNOPSIS
+  Installs various PowerShell modules.
+
+.DESCRIPTION
+  This script is used to install various PowerShell modules that I find useful.
+
+.EXAMPLE
+  .\powershell.ps1
+
+.NOTES
+  File Name: powershell.ps1
+  Author: PatEvs (https://github.com/patevs)
+  Last Edit: 24/04/2020 - April 24th 2020
+
+.LINK
+  Repository:
+    * https://github.com/patevs/dotfiles
+  Script:
+    * https://github.com/patevs/dotfiles/blob/master/deps/components/powershell.ps1
+#>
+
+# EOF #
