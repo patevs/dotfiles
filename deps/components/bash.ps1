477be10f5634af9b5c11446910d8ef50ea3b4338
Patrick Evans <ppevans11@gmail.com>
2020-04-24 19:50:48 +1200

    :sparkles: Created deps/components/bash.ps1

---
+++ deps/components/bash.ps1
@@ -0,0 +1,26 @@
+<#
+
+.SYNOPSIS
+  Installs various Bash scripts.
+
+.DESCRIPTION
+  This script is used to install various Bash scripts that I find useful.
+
+.EXAMPLE
+  .\bash.ps1
+
+.NOTES
+  File Name: bash.ps1
+  Author: PatEvs (https://github.com/patevs)
+  Last Edit: 24/04/2020 - April 24th 2020
+
+.LINK
+  Repository:
+    * https://github.com/patevs/dotfiles
+  Script:
+    * https://github.com/patevs/dotfiles/blob/master/deps/components/bash.ps1
+#>
+
+Write-Host "bash.ps1"
+
+# EOF #
