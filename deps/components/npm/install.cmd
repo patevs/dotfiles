34a5418217854e0af9075331b9793075c2da9a49
Patrick Evans <ppevans11@gmail.com>
2020-04-26 15:56:44 +1200

    :sparkles: Added husky to deps/components/npm/install.cmd

--- deps/components/npm/install.cmd
+++ deps/components/npm/install.cmd
@@ -81,6 +81,7 @@ CALL npm install --global g3l
 CALL npm install --global gitmoji-cli
 :: # https://github.com/himynameisdave/git-labelmaker
 :: CALL npm install --global git-labelmaker
+CALL npm install --global husky
 :: # https://github.com/yyx990803/starz
 :: CALL npm install --global starz
 :: # https://github.com/FredrikNoren/ungit
