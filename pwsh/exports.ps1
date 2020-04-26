87e2c69be7b9ba7bc129762fc6436000710f3ec8
Patrick Evans <ppevans11@gmail.com>
2020-04-26 14:57:40 +1200

    :bulb: Updated comments in pwsh/exports.ps1

--- pwsh/exports.ps1
+++ pwsh/exports.ps1
@@ -2,6 +2,15 @@
 # exports.ps1 #
 # ----------- #
 
+# TODO: Add exports for;
+
+# ANDROID_HOME, ANDROID_SDK, ANDROID_SDK_ROOT, ANDROID_NDK
+# CODE_HOME
+# GIT_HOME
+# JAVA_HOME
+
+Set-Environment "GIT_HOME" "C:\Program Files\Git"
+
 # Make vim the default editor
 # Set-Environment "EDITOR" "gvim --nofork"
 # Set-Environment "GIT_EDITOR" $Env:EDITOR
