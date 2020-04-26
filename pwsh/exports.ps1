75805e8b9c72f5de109e59fcb42056efe9fd1b0a
Patrick Evans <ppevans11@gmail.com>
2020-04-26 15:20:54 +1200

    :bulb: Updated comments in pwsh/exports.ps1

--- pwsh/exports.ps1
+++ pwsh/exports.ps1
@@ -2,14 +2,7 @@
 # exports.ps1 #
 # ----------- #
 
-# TODO: Add exports for;
-
-# ANDROID_HOME, ANDROID_SDK, ANDROID_SDK_ROOT, ANDROID_NDK
-# CODE_HOME
-# GIT_HOME
-# JAVA_HOME
-
-Set-Environment "TEST_VAR" "C:\Users\Pat"
+# Write-Host "exports.ps1"
 
 Set-Environment "ANDROID_HOME" "C:\Users\Pat\AppData\Local\Android\Sdk"
 Set-Environment "ANDROID_SDK" "C:\Users\Pat\AppData\Local\Android\Sdk"
@@ -21,6 +14,18 @@ Set-Environment "CODE_HOME" "C:\Program Files\Microsoft VS Code"
 Set-Environment "GIT_HOME" "C:\Program Files\Git"
 
 Set-Environment "HERMES_WS_DIR"  "C:\Users\Pat\git\dev\hermes_ws"
+
+# Set-Environment "JAVA_HOME" "..."
+
+# MSBuild
+# C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin
+
+# TODO: Add to path:
+# ANDROID_HOME\platform-tools
+# ANDROID_HOME\emulator
+# CODE_HOME\bin
+# GIT_HOME\bin
+# GIT_HOME\cmd
 
 # Make vim the default editor
 # Set-Environment "EDITOR" "gvim --nofork"
