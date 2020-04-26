18bac430c66a6534de6f20f3f2011a419ead3d50
Patrick Evans <ppevans11@gmail.com>
2020-04-26 15:00:44 +1200

    :sparkles: Updated pwsh/exports.ps1

--- pwsh/exports.ps1
+++ pwsh/exports.ps1
@@ -9,7 +9,16 @@
 # GIT_HOME
 # JAVA_HOME
 
+Set-Environment "TEST_VAR" "C:\Users\Pat"
+
+Set-Environment "ANDROID_HOME" "C:\Users\Pat\AppData\Local\Android\Sdk"
+Set-Environment "ANDROID_NDK" "C:\Users\Pat\AppData\Local\Android\Sdk\ndk\21.1.6352462"
+
+Set-Environment "CODE_HOME" "C:\Program Files\Microsoft VS Code"
+
 Set-Environment "GIT_HOME" "C:\Program Files\Git"
+
+Set-Environment "HERMES_WS_DIR"  "C:\Users\Pat\git\dev\hermes_ws"
 
 # Make vim the default editor
 # Set-Environment "EDITOR" "gvim --nofork"
