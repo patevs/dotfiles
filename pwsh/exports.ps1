e33c19a7cdc8a90e88db37bcca123f78be0236ce
Patrick Evans <ppevans11@gmail.com>
2020-04-27 13:54:42 +1200

    :sparkles: Updated pwsh/exports.ps1

--- pwsh/exports.ps1
+++ pwsh/exports.ps1
@@ -16,14 +16,14 @@ Set-Environment "CODE_HOME" "C:\Program Files\Microsoft VS Code"
 # Git
 Set-Environment "GIT_HOME" "C:\Program Files\Git"
 
-# Hermes
+# Hermes Engine
 Set-Environment "HERMES_WS_DIR"  "C:\Users\Pat\git\dev\hermes_ws"
 
-# Java
-# Set-Environment "JAVA_HOME" "..."
+# JDK
+Set-Environment "JAVA_HOME" "C:\Program Files\Java\jdk1.8.0_211"
 
 # MSBuild
-Set-Environment "MSBUILD" "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin"
+Set-Environment "MSBUILD_HOME" "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current"
 
 # Watchman
 Set-Environment "WATCHMAN_HOME" "C:\tools\watchman"
@@ -38,7 +38,8 @@ AppendEnvPath "$env:ANDROID_HOME\emulator"
 AppendEnvPath "$env:CODE_HOME\bin"
 AppendEnvPath "$env:GIT_HOME\bin"
 AppendEnvPath "$env:GIT_HOME\cmd"
-AppendEnvPath "$env:MSBUILD"
+AppendEnvPath "$env:JAVA_HOME\bin"
+AppendEnvPath "$env:MSBUILD_HOME\bin"
 AppendEnvPath "$env:WATCHMAN_HOME\bin"
 
 # Disable the Progress Bar
