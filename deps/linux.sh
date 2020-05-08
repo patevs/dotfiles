9b42deb0a7906080ccb872d478a2b9cfd0f1b17f
Patrick Evans <ppevans11@gmail.com>
2020-05-08 17:59:26 +1200

    :sparkles: Updated deps/linux.sh

--- deps/linux.sh
+++ deps/linux.sh
@@ -14,4 +14,16 @@
 # Install Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
+# Install the Homebrew dependencies
+sudo apt-get install build-essential
+
+# Configure Homebrew in your /home/pat/.profile by running
+echo 'eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)' >> /home/pat/.profile
+
+# Add Homebrew to your PATH
+eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+
+# Install GCC
+brew install gcc
+
 # EOF #
