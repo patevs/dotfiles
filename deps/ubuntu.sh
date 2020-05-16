b86797a16afc188236244c0bfd043d70020180e7
Patrick Evans <ppevans11@gmail.com>
2020-05-16 14:30:39 +1200

    :recycle: Formatting deps/ubuntu.sh

--- deps/ubuntu.sh
+++ deps/ubuntu.sh
@@ -39,8 +39,8 @@ sudo apt upgrade -y
 
 sudo apt install neofetch
 sudo apt install neovim
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-      https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+# sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+#       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 # ------------------------------------------------------------------------------------------------------- #
 
