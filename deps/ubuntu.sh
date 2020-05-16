ccb20b132f6cffe09c393dffe2a7e178a079be03
Patrick Evans <ppevans11@gmail.com>
2020-05-16 16:48:07 +1200

    :sparkles: Updated deps/ubuntu.sh

--- deps/ubuntu.sh
+++ deps/ubuntu.sh
@@ -82,6 +82,19 @@ sudo apt install firefox
 # Developer Tools
 # ===============
 
+# Python3
+sudo apt install python3
+# Pip
+sudo apt install python3-pip
+
+# Python Version Manager
+# git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+# echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+# echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+# echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.bashrc
+# Restart Shell
+# exec "$SHELL"
+
 # Node Version Manager
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 
