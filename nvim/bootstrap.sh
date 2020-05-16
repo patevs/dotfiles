038d99d5d593b04e0001472aa4fdfb7b0716189c
Patrick Evans <ppevans11@gmail.com>
2020-05-16 14:20:30 +1200

    :sparkles: Updated nvim/bootstrap.sh

--- nvim/bootstrap.sh
+++ nvim/bootstrap.sh
@@ -4,14 +4,12 @@
 # bootstrap.sh #
 # ------------ #
 
-# /home/pat/.config/nvim/init.vim
-
 # Install Vim-Plug
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 # Copy init.vim config
-cp init.vim /home/pat/.config/nvim/init.vim
+cp -f init.vim /home/pat/.config/nvim/init.vim
 
 # Install Plugins
 nvim +PlugInstall
