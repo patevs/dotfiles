b5bd664ca0dfa89c1fcd4466f7fcad86ab3a5980
Patrick Evans <ppevans11@gmail.com>
2020-06-03 13:40:37 +1200

    :bulb: Added comments to nvim/bootstrap.sh

--- nvim/bootstrap.sh
+++ nvim/bootstrap.sh
@@ -4,6 +4,8 @@
 # bootstrap.sh #
 # ------------ #
 
+# TODO: Detect OS type and ensure running on linux
+
 # Install Vim-Plug
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
