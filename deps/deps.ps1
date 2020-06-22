6e7222a052ffce2ebbbb5bddc6bdeef967e15b18
Patrick Evans <ppevans11@gmail.com>
2020-06-22 14:45:48 +1200

    :sparkles: Added logitech-gaming-software-np to deps/deps.ps1

--- deps/deps.ps1
+++ deps/deps.ps1
@@ -266,6 +266,7 @@ scoop install ccleaner
 # scoop install etcher
 # scoop install imagemagick
 scoop install inkscape
+scoop install logitech-gaming-software-np
 scoop install powertoys
 # scoop install rufus
 scoop install snappy-driver-installer-origin
