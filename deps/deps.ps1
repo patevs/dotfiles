39952505e6fe7c49b7d18b9e6d23cfebcd2253fc
Patrick Evans <ppevans11@gmail.com>
2020-06-22 14:42:54 +1200

    :sparkles: Added sublime-text to deps/deps.ps1

--- deps/deps.ps1
+++ deps/deps.ps1
@@ -271,6 +271,7 @@ scoop install vncviewer
 # scoop install rufus
 scoop install snappy-driver-installer-origin
 scoop install speccy
+scoop install sublime-text
 scoop install sumatrapdf
 scoop install teracopy-np
 # scoop install vscodium
