555f96de90a2a3fb6367d304020a4ad347534f57
Patrick Evans <ppevans11@gmail.com>
2020-08-19 16:56:28 +1200

    :heavy_plus_sign: Added react-devtools to npm/install.cmd

--- npm/install.cmd
+++ npm/install.cmd
@@ -275,12 +275,13 @@ CALL npm install --global tape-cli
 :: ---------------------------------------------------------------------------- ::
 
 :: React Development Utilities
-:: ECHO. & ECHO  [44m Installing React Development Utility Modules: [0m & ECHO.
+ECHO. & ECHO  [44m Installing React Development Utility Modules: [0m & ECHO.
 
 :: # https://github.com/facebook/create-react-app
 :: CALL npm install --global create-react-app
 :: # https://github.com/diegohaz/generact
 :: CALL npm install --global generact
+CALL npm install --global react-devtools
 
 :: ---------------------------------------------------------------------------- ::
 
