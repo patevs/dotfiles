e428e4e254f5697710239167821c066337064f04
Patrick Evans <ppevans11@gmail.com>
2020-09-13 16:03:02 +1200

    :sparkles: Updated bash/bootstrap.sh to download .git-prompt.sh

--- bash/bootstrap.sh
+++ bash/bootstrap.sh
@@ -8,6 +8,11 @@
 # The script copies '.bash_aliases', '.bash_profile', '.bashrc', and '.profile'
 # into my home directory.
 
+# Posh-Git-Sh
+# TODO: Ensure curl command exists
+curl https://raw.githubusercontent.com/lyze/posh-git-sh/master/git-prompt.sh -o ~/.git-prompt.sh
+# wget https://raw.githubusercontent.com/lyze/posh-git-sh/master/git-prompt.sh -O ~/.git-prompt.sh
+
 cp .bash_aliases ~/.bash_aliases
 cp .bash_profile ~/.bash_profile
 cp .bashrc ~/.bashrc
